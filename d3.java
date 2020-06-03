978501fe4afeff9b63c57e90509822560b2a5961
Sameer <sameer1771999@gmail.com>
2020-06-03 17:12:43 +0530

    d3

---
+++ d3.java
@@ -0,0 +1,19 @@
+Two City Scheduling
+
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        Comparator<int[]> comparator = (a, b) -> Math.abs(b[0] - b[1]) - Math.abs(a[0] - a[1]);
+        Arrays.sort(costs, comparator);
+        int N = costs.length / 2,c1 = 0,c2= 0,ans = 0,i = 0;
+        while (i < 2 * N) {
+            if ((costs[i][0] <= costs[i][1] && c1 < N) || c2 == N) {
+                ans += costs[i++][0];
+                c1++;
+            } else {
+                ans += costs[i++][1];
+                c2++;
+            }
+        }
+        return ans;
+    }
+}
